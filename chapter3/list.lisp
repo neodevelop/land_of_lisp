ec7ae4cd6a35de16d3acef3f550d7596a6400542
neodevelop <juan@makingdevs.com>
2018-09-23 20:28:58 -0500

    CH3 eat that lists

---
+++ chapter3/list.lisp
@@ -0,0 +1,8 @@
+(car '((peas carrots tomatoes) (pork beef chicken)))
+(cdr '(peas carrots tomatoes))
+(cdr (car '((peas carrots tomatoes) (pork beef chicken))))
+(cdar '((peas carrots tomatoes) (pork beef chicken)))
+(cddr '((peas carrots tomatoes) (pork beef chicken) duck))
+(caddr '((peas carrots tomatoes) (pork beef chicken) duck))
+(cddar '((peas carrots tomatoes) (pork beef chicken) duck))
+(cadadr '((peas carrots tomatoes) (pork beef chicken) duck))
